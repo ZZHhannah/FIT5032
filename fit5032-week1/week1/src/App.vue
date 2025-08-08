cf68e27c60db8b93edf62c03bf42ad40d05f4fbc
Zihan <zzha0495@student.monash.edu>
2025-08-08 20:40:51 +1000

    add fit5032-week1/week1 files

---
+++ fit5032-week1/week1/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from './components/Zihan.vue'
+</script>
+
+<template>
+ <TheWelcome/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ fit5032-week1/week1/src/components/Zihan.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+<h1>Welcome fit 5032 week1</h1>
+</template>
